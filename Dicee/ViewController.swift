0241b6eaa8b94d67c3182932b2bd96b8ad3108f8
Pavan Sabnis <pvnsbns@gmail.com>
2018-01-27 17:07:22 -0800

    Hooked up the IBOutlets and IBActions required for this app.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
